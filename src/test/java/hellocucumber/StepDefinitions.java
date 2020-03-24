59be4053bd112c5b641f4745e457d14778c432e6
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2020-03-23 21:19:41 -0700

    Use variables and multiple examples

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -16,9 +16,9 @@ public class StepDefinitions {
     private String today;
     private String actualAnswer;
 
-    @Given("today is Sunday")
-    public void today_is_Sunday() {
-        today = "Sunday";
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
     }
 
     @When("I ask whether it's Friday yet")
